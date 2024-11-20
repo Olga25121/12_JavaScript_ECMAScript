1d6a5fda30f19ff5fbfdcadd0dc9655569dd010e
Olga <olgajensen@mail.ru>
2024-11-20 23:53:39 +0300

    initial commit

---
+++ Homework/Task_7/data.js
@@ -0,0 +1,58 @@
+﻿const dataProducts = `
+[
+    {
+        "id":1,
+        "img":"./img/Image1.png",
+        "subtitle":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        "add":"@",
+        "addText":"Add to Cart"
+    },
+    {
+        "id":2,
+        "img":"./img/Image2.png",
+        "subtitle":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        "add":"@",
+        "addText":"Add to Cart"
+    },
+    {
+        "id":3,
+        "img":"./img/Image3.png",
+        "subtitle":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        "add":"@",
+        "addText":"Add to Cart"
+    },
+    {
+        "id":4,
+        "img":"./img/Image4.png",
+        "subtitle":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        "add":"@",
+        "addText":"Add to Cart"
+    },
+    {
+        "id":5,
+        "img":"./img/Image5.png",
+        "subtitle":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        "add":"@",
+        "addText":"Add to Cart"
+    },
+    {
+        "id":6,
+        "img":"./img/Image6.png",
+        "subtitle":"ELLERY X M'O CAPSULE",
+        "text":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        "add":"@",
+        "addText":"Add to Cart"
+    }
+
+] `
